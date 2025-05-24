f7425d4b459ceca80945f1fa6c16ec4a9c55ac24
Mateusz Falkiewicz <mateuszfalkiewicz@protonmail.com>
2025-05-24 21:54:33 +0200

    Improved deck system, language changed to Polish

--- main.cpp
+++ main.cpp
@@ -3,21 +3,39 @@
 
 using namespace std;
 
-vector<int> deck = {1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
-vector<int> active_cards = {};
+struct Card {
+    string name;
+    int value;
+};
+
+vector<Card> deck = {{"2 kier", 2},{"2 pik", 2}, {"2 karo", 2}, {"2 trefl", 2},
+{"3 kier", 3},{"3 pik", 3}, {"3 karo", 3}, {"3 trefl", 3},
+{"4 kier", 4},{"2 pik", 4}, {"4 karo", 4}, {"4 trefl", 4},
+{"5 kier", 5},{"5 pik", 5}, {"5 karo", 5}, {"5 trefl", 5},
+{"6 kier", 6},{"6 pik", 6}, {"6 karo", 6}, {"6 trefl", 6},
+{"7 kier", 7},{"7 pik", 7}, {"7 karo", 7}, {"7 trefl", 7},
+{"8 kier", 8},{"8 pik", 8}, {"7 karo", 8}, {"8 trefl", 8},
+{"9 kier", 9},{"9 pik", 9}, {"9 karo", 9}, {"9 trefl", 9},
+{"10 kier", 10},{"10 pik", 10}, {"10 karo", 10}, {"10 trefl", 10},
+{"Walet kier", 11},{"Walet pik", 11}, {"Walet karo", 11}, {"Walet trefl", 11},
+{"Dama kier", 12},{"Dama pik", 12}, {"Dama karo", 12}, {"Dama trefl", 12},
+{"Król kier", 13},{"Król pik", 13}, {"Król karo", 13}, {"Król trefl", 13},
+{"As kier", 14},{"As pik", 14}, {"As karo", 14}, {"As trefl", 14},};
+vector<int> deck_old = {1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
+vector<Card> active_cards = {};
 
 int choice;
-int card_in_play;
-int dealt_card;
+Card card_in_play;
+Card dealt_card;
 int score;
 int active_deck_size;
 
 bool lost = false;
 bool playing = true;
 
-int dealCard(vector<int>& vec, int n) {
+Card dealCard(vector<Card>& vec, int n) {
     swap(vec[n], vec.back());
-    int card = vec.back();
+    Card card = vec.back();
     vec.pop_back();
 
     return card;
@@ -25,44 +43,44 @@ int dealCard(vector<int>& vec, int n) {
 
 int main() {
     while (playing) {
-        cout << "Welcome to High-Low! Type 1. to play, type 2. to exit." << endl;
+        lost = false;
+        cout << "Witaj w High-Low! Wybierz 1. by zagrać, wybierz 2. by wyjść." << endl;
         cin >> choice;
         switch (choice) {
             case 1:
                 while (!lost) {
-                    if (active_cards.empty()) {
+                    if (active_cards.empty()) { // fix!!
                         active_cards = deck;
                         active_deck_size = 52;
-                        cout << "The deck has been shuffled!" << endl;
+                        cout << "Przetasowano talię" << endl;
 
                         card_in_play = dealCard(active_cards, rand() % active_deck_size);
                         active_deck_size--;
                     }
 
-
-                    cout << "The card in play is of value: " << card_in_play << endl;
+                    cout << "Twoja karta: " << card_in_play.name << endl;
                     invalid_input:
-                    cout << "What is your bet? (1. for higher, 2. for lower)" << endl;
+                    cout << "Jak obstawiasz? (1. jeżeli wyżej, 2. jeżeli niżej)" << endl;
                     cin >> choice;
                     switch (choice) {
                         case 1:
                             dealt_card = dealCard(active_cards, rand() % active_deck_size);
                             active_deck_size--;
-                            cout << "You were dealt a card of value: " << dealt_card << endl;
+                            cout << "Następna karta to: " << dealt_card.name << endl;
 
-                            if (dealt_card == card_in_play) {
-                                cout << "Draw! No points" << endl;
+                            if (dealt_card.value == card_in_play.value) {
+                                cout << "Remis! Bez punktów" << endl;
                                 card_in_play = dealt_card;
                                 break;
                             }
-                            if (dealt_card > card_in_play) {
-                                cout << "Score! +1 points" << endl;
+                            if (dealt_card.value > card_in_play.value) {
+                                cout << "Zdobywasz 1 punkt!" << endl;
                                 score++;
                                 card_in_play = dealt_card;
                                 break;
                             }
-                            if (dealt_card < card_in_play) {
-                                cout << "You lose! Final score: " << score << endl;
+                            if (dealt_card.value < card_in_play.value) {
+                                cout << "Przegrywasz! Twój wynik to: " << score << " punktów" << endl;
                                 lost = true;
                                 break;
                             }
@@ -70,24 +88,25 @@ int main() {
                         case 2:
                             dealt_card = dealCard(active_cards, rand() % active_deck_size);
                             active_deck_size--;
-                            cout << "You were dealt a card of value: " << dealt_card << endl;
+                            cout << "Następna karta to: " << dealt_card.name << endl;
 
-                            if (dealt_card == card_in_play) {
-                                cout << "Draw! No points" << endl;
+                            if (dealt_card.value == card_in_play.value) {
+                                cout << "Remis! Bez punktów" << endl;
                                 card_in_play = dealt_card;
                                 break;
                             }
-                            if (dealt_card < card_in_play) {
-                                cout << "Score! +1 points" << endl;
+                            if (dealt_card.value < card_in_play.value) {
+                                cout << "Zdobywasz 1 punkt!" << endl;
                                 score++;
                                 card_in_play = dealt_card;
                                 break;
                             }
-                            if (dealt_card > card_in_play) {
-                                cout << "You lose! Final score: " << score << endl;
+                            if (dealt_card.value > card_in_play.value) {
+                                cout << "Przegrywasz! Twój wynik to: " << score << " punktów" << endl;
                                 lost = true;
                                 break;
                             }
+
                         default:
                             goto invalid_input;
                     }
